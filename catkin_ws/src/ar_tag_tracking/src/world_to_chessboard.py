d7d48b43de36a96d89d8d8d7022117b0e6679216
Noah Whelan <noah.whelan@berkeley.edu>
2024-12-05 17:02:55 -0800

    Added other transform

---
+++ catkin_ws/src/ar_tag_tracking/src/world_to_chessboard.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+
+def calculate_world_to_chessboard():
+    rospy.init_node('transform_listener')
+
+    listener = tf.TransformListener()
+
+    try:
+        rospy.loginfo("Waiting for transforms...")
+
+        # Wait for the transform to become available
+        listener.waitForTransform('world', 'chessboard', rospy.Time(), rospy.Duration(4.0))
+
+        # Get the transform
+        (trans, rot) = listener.lookupTransform('world', 'chessboard', rospy.Time(0))
+        rospy.loginfo(f"World to Chessboard: Translation={trans}, Rotation={rot}")
+
+    except tf.Exception as e:
+        rospy.logerr(f"Error calculating transform: {e}")
+
+if __name__ == "__main__":
+    calculate_world_to_chessboard()
