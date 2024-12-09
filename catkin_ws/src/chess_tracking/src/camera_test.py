343ec90c404fcb000181b7a25438511a2219e064
Sylvester Seo <sylvesterseo@berkeley.edu>
2024-12-08 18:40:02 -0800

    update camera

--- catkin_ws/src/chess_tracking/src/camera_test.py
+++ catkin_ws/src/chess_tracking/src/camera_test.py
@@ -67,6 +67,89 @@ class CameraTest:
                 cv2.circle(image, (x, y), r, (0, 255, 0), 4)
 
         return bridge.cv2_to_imgmsg(image)
+    
+    def test_new(self, img):
+
+        # Load the image
+        image = cv2.imread('IMG_7381.png')
+
+        # Convert the image from BGR to HSV
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        _, binary_image = cv2.threshold(hsv, 127, 255, cv2.THRESH_BINARY)
+
+        ret, corners = cv2.findChessboardCorners(hsv, (7,7),None)
+
+        # print(np.shape(corners))
+        corners = corners.squeeze()
+        # print(corners)
+
+        ### CALIBRATION MEASURES ###
+        # # average rate of change
+        # horizontal_incs = [np.array([0.0, 0.0]) for _ in range(7)]
+        # vertical_incs = [np.array([0.0, 0.0]) for _ in range(7)]
+        # diagonal_incs = [np.array([0.0, 0.0]), np.array([0.0, 0.0])]
+
+        # for i in range(49):
+        #     corner = corners[i]
+        #     if i % 7 >= 2: # horizontal
+        #         horizontal_incs[i//7] += (corners[i] - corners[i - 1]) - (corners[i - 1] - corners[i - 2])
+        #     if i >= 14: # vertical
+        #         vertical_incs[i % 7] += (corners[i] - corners[i - 7]) - (corners[i - 7] - corners[i - 14])
+        #     if i % 8 == 0 and i >= 16: # left diagonal
+        #         diagonal_incs[0] += (corners[i] - corners[i - 8]) - (corners[i-8] - corners[i - 16])
+        #     if i % 6 == 0 and 18 <= i <= 42: # left diagonal
+        #         diagonal_incs[1] += (corners[i] - corners[i - 6]) - (corners[i-6] - corners[i - 12])
+        # horizontal_incs = np.divide(horizontal_incs, 5)
+        # vertical_incs = np.divide(vertical_incs, 5)
+        # diagonal_incs = np.divide(diagonal_incs, 5)
+
+        # top left corner
+        corner = corners[0] + corners[0] - corners[8]
+        cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+
+        # top right corner
+        corner = corners[6] + corners[6] - corners[12]
+        cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+
+        # bottom left corner
+        corner = corners[42] + corners[42] - corners[36]
+        cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+        
+        # bottom right corner
+        corner = corners[48] + corners[48] - corners[40]
+        cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+
+        for i in range(49):
+            color = (0, 255, 0)
+            if i // 7 == 0: # first row
+                first_row_x = int(2 * corners[i][0] - corners[i+7][0])
+                first_row_y = int(2 * corners[i][1] - corners[i+7][1])
+                cv2.circle(image, (first_row_x, first_row_y), 30, color, 4)  # Draw the circle
+            
+            elif i // 7 == 6:
+                bottom_row_x = int(2 * corners[i][0] - corners[i-7][0])
+                bottom_row_y = int(2 * corners[i][1] - corners[i-7][1])
+                cv2.circle(image, (bottom_row_x, bottom_row_y), 30, color, 4)  # Draw the circle
+            
+            if i % 7 == 0: # left edge
+                left_col_x = int(2*corners[i][0] - corners[i+1][0])
+                left_col_y = int(2*corners[i][1] - corners[i+1][1])
+                cv2.circle(image, (left_col_x, left_col_y), 30, color, 4)  # Draw the circle
+
+            if i % 7 == 6: # right edge
+                right_col_x = int(2*corners[i][0] - corners[i-1][0])
+                right_col_y = int(2*corners[i][1] - corners[i-1][1])
+                cv2.circle(image, (right_col_x, right_col_y), 30, color, 4)  # Draw the circle
+
+
+            # print(corner, type(corner))
+            x, y = int(corners[i][0]), int(corners[i][1])
+            cv2.circle(image, (x, y), 30, color, 4)  # Draw the circle
+            cv2.putText(image, f"{i}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 3, color, 2)
+            cv2.putText(image, f"X: {x} Y: {y}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+        print(x, y)
 
 
 if __name__ == "__main__":
