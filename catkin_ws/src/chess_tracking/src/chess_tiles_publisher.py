02251f56411b4cbdd7b35ebbf99ba61d47b5b05b
noah-whelann <noah.whelan@berkeley.edu>
2024-12-10 20:52:53 -0800

    Updated more

--- catkin_ws/src/chess_tracking/src/chess_tiles_publisher.py
+++ catkin_ws/src/chess_tracking/src/chess_tiles_publisher.py
@@ -41,7 +41,7 @@ class ChessTileCoordinates:
         processed_img, corners = self.process_board(msg)
 
         self.image_pub.publish(processed_img)
-
+        
         #A1 [0.439, -0.061, -0.156]
         #H1 [0.835, -0.005, -0.161]
         #H8 [0.775, 0.391, -0.146]
@@ -88,52 +88,61 @@ class ChessTileCoordinates:
         if ret:
             corners = corners.squeeze()
 
+            all_corners = np.empty((0, 2))
+
             # top left corner
             corner = corners[0] + corners[0] - corners[8]
             cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+            all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
 
             # top right corner
             corner = corners[6] + corners[6] - corners[12]
             cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+            all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
 
             # bottom left corner
             corner = corners[42] + corners[42] - corners[36]
             cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+            all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
             
             # bottom right corner
             corner = corners[48] + corners[48] - corners[40]
             cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+            all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
 
             for i in range(49):
                 color = (0, 255, 0)
                 if i // 7 == 0: # first row
-                    first_row_x = int(2 * corners[i][0] - corners[i+7][0])
-                    first_row_y = int(2 * corners[i][1] - corners[i+7][1])
-                    cv2.circle(image, (first_row_x, first_row_y), 30, color, 4)  # Draw the circle
+                    corner = 2 * corners[i] - corners[i+7]
+                    cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+                    all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
                 
                 elif i // 7 == 6:
-                    bottom_row_x = int(2 * corners[i][0] - corners[i-7][0])
-                    bottom_row_y = int(2 * corners[i][1] - corners[i-7][1])
-                    cv2.circle(image, (bottom_row_x, bottom_row_y), 30, color, 4)  # Draw the circle
+                    corner = 2 * corners[i] - corners[i-7]
+                    cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+                    all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
                 
                 if i % 7 == 0: # left edge
-                    left_col_x = int(2*corners[i][0] - corners[i+1][0])
-                    left_col_y = int(2*corners[i][1] - corners[i+1][1])
-                    cv2.circle(image, (left_col_x, left_col_y), 30, color, 4)  # Draw the circle
+                    corner = 2 * corners[i] - corners[i+1]
+                    cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+                    all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
 
                 if i % 7 == 6: # right edge
-                    right_col_x = int(2*corners[i][0] - corners[i-1][0])
-                    right_col_y = int(2*corners[i][1] - corners[i-1][1])
-                    cv2.circle(image, (right_col_x, right_col_y), 30, color, 4)  # Draw the circle
+                    corner = 2 * corners[i] - corners[i-1]
+                    cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+                    all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
 
-                x, y = int(corners[i][0]), int(corners[i][1])
-                cv2.circle(image, (x, y), 30, color, 4)  # Draw the circle
+                corner = corners[i]
+                cv2.circle(image, (int(corner[0]), int(corner[1])), 30, (0, 0, 255), 4)  # Draw the circle
+                all_corners = np.append(all_corners, corner.reshape(1, -1), axis=0)
+
+            all_corners = all_corners.tolist()
 
         else:
             print("NOT FOUND")
             corner = [0, 0]
         
-        return bridge.cv2_to_imgmsg(image), corners
+        return bridge.cv2_to_imgmsg(image), all_corners
 
 if __name__ == "__main__":
     ChessTileCoordinates()
@@ -189,5 +198,10 @@ if __name__ == "__main__":
  [1111.9459   900.708  ]
  [1235.5393   906.8279 ]
  [1359.1764   913.14154]]
+
+
+
+ [[546.5472412109375, 18.122955322265625], [1553.020263671875, 60.0533447265625], [505.2271728515625, 1004.2926635742188], [1486.28564453125, 1065.3271484375], [670.6596069335938, 24.98480224609375], [540.8052978515625, 144.1320037841797], [665.1450805664062, 149.99072265625], [795.226806640625, 29.84039306640625], [789.48486328125, 155.8494415283203], [920.4722290039062, 36.161834716796875], [914.0362548828125, 162.06967163085938], [1041.7049560546875, 42.52813720703125], [1037.0, 168.0], [1173.18017578125, 46.14617919921875], [1164.467529296875, 172.2851104736328], [1299.571044921875, 53.8868408203125], [1290.7852783203125, 179.48536682128906], [1427.1798095703125, 55.33673095703125], [1544.2344970703125, 185.65187072753906], [1417.5098876953125, 182.56861877441406], [535.5181884765625, 268.1347961425781], [659.6305541992188, 274.99664306640625], [783.742919921875, 281.8584899902344], [907.6002807617188, 287.9775085449219], [1032.2950439453125, 293.47186279296875], [1155.7548828125, 298.4240417480469], [1281.99951171875, 305.0838928222656], [1533.680419921875, 314.5171203613281], [1407.8399658203125, 309.8005065917969], [530.0355224609375, 394.3231201171875], [653.6728515625, 400.2030334472656], [777.3101806640625, 406.08294677734375], [901.112060546875, 412.697265625], [1024.970703125, 419.15313720703125], [1149.5826416015625, 425.0092468261719], [1275.947509765625, 431.50457763671875], [1526.871337890625, 441.2618408203125], [1401.409423828125, 436.3832092285156], [523.7191162109375, 515.3825073242188], [647.7335205078125, 522.6635131835938], [771.7479248046875, 529.9445190429688], [895.2698364257812, 536.7974853515625], [1017.65087890625, 543.7203979492188], [1142.193115234375, 548.7980346679688], [1267.1497802734375, 555.5639038085938], [1519.6322021484375, 569.3359985351562], [1393.3909912109375, 562.449951171875], [519.167236328125, 639.2747192382812], [642.6317749023438, 646.0523071289062], [766.0963134765625, 652.8298950195312], [888.9268798828125, 659.2049560546875], [1011.9498901367188, 666.01220703125], [1135.528076171875, 672.4595336914062], [1259.861572265625, 678.7305297851562], [1510.105712890625, 693.2182006835938], [1384.983642578125, 685.974365234375], [514.0538330078125, 760.3330688476562], [637.1481323242188, 767.7413940429688], [760.242431640625, 775.1497192382812], [883.39599609375, 782.2955932617188], [1006.1826171875, 787.8327026367188], [1128.979248046875, 795.0750122070312], [1252.968505859375, 801.9227905273438], [1501.88427734375, 816.1925659179688], [1377.4263916015625, 809.0576782226562], [628.3214721679688, 1011.7009887695312], [510.296875, 881.876220703125], [632.7348022460938, 889.72119140625], [750.10302734375, 1019.9826049804688], [755.1727294921875, 897.566162109375], [868.800537109375, 1028.974609375], [876.0982666015625, 905.6350708007812], [990.63134765625, 1032.544921875], [998.406982421875, 910.1888427734375], [1116.0322265625, 1040.60546875], [1122.5057373046875, 917.8402709960938], [1239.623291015625, 1047.734375], [1246.2958984375, 924.82861328125], [1361.8277587890625, 1058.192138671875], [1492.958251953125, 942.4212646484375], [1369.6270751953125, 933.6249389648438]]
+
 
 '''
