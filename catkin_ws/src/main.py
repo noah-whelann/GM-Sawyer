146d425341ee64eeaf280ec760513d4d5dbdc3b7
Noah Whelan <noah.whelan@berkeley.edu>
2024-12-11 01:16:10 -0800

    Updated more main

--- catkin_ws/src/main.py
+++ catkin_ws/src/main.py
@@ -2,14 +2,18 @@
 # Step 1:
 # Define piece pickup z coordinate (lowest point robot should go/pick up piece at)
 #
+from catkin_ws.src.chess_tracking.src import transform_coordinates_service
 from stockfish import Stockfish
 from planning.chessboard import ChessBoard, TileObject
+import rospy
+from geometry_msgs.msg import Point, PointStamped
 
 
 def get_board_state():  # return fen of current board state
     return
 
 
+# should return a Point() in terms of pixels
 def get_piece_location_on_tile(tile):
     return
 
@@ -23,19 +27,30 @@ def get_tile_locations():  # returns a list of tuples, each tuple is for a tile,
 
 
 def move_robot(move_location):  # transform pixel coordinates to world coordinates
-    transformed_coords = transformed_coordinates_service(move_location)
-    move_arm_service(transformed_coords)
+    rospy.wait_for_service("transform_coordinates_service")
+    try:
+        transform_service = rospy.ServiceProxy("transform_coordinates_service")
+
+        transform_response = transform_service(move_location)
+
+        move_arm_function(transform_response)  # sends in a PointStamped type
+
+    except:
+        rospy.ServiceException as e:
+        rospy.logerr("Service call failed: %s", e)
     # send over the world coordinates to move_arm function
     # that handles all the ik and planning
     return
+
 
 def pickup_and_place_piece(from_tile, to_tile):
     return
 
 
-
-
 def main():
+
+    rospy.init_node("main_node", anonymous=True)
+
     stockfish_path = ".."
     stockfish = Stockfish(stockfish_path)  # init Stockfish
     stockfish.set_skill_level(5)
@@ -59,7 +74,8 @@ def main():
         pickup_tile_coords = get_piece_location_on_tile(pickup_tile)
         place_tile_coords = tuple(place_tile.x, place_tile.y)
 
-        pickup_and_place_piece(pickup_tile_coords, place_tile_coords) # should be one fluid motion
+        # should be one fluid motion
+        pickup_and_place_piece(pickup_tile_coords, place_tile_coords)
         # robot then tucks after its move
 
         # wait for user to execute move
